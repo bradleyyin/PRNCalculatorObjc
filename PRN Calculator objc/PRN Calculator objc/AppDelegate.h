e230c7bbc0c80ace0be30425959287ea65f61052
bradleyyin <bradleyyin@gmail.com>
2019-10-10 13:03:01 -0700

    setup storyboard

---
+++ PRN Calculator objc/PRN Calculator objc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PRN Calculator objc
+//
+//  Created by Bradley Yin on 10/10/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
